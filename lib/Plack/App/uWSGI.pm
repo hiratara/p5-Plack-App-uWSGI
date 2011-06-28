e906dd17aa45aa1f585961186680cd95f0cf70b9
hiratara <hiratara@cpan.org>
2011-06-28 09:12:42 +0900

    initial commit

---
+++ lib/Plack/App/uWSGI.pm
@@ -0,0 +1,32 @@
+package Plack::App::uWSGI;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Plack::App::uWSGI -
+
+=head1 SYNOPSIS
+
+  use Plack::App::uWSGI;
+
+=head1 DESCRIPTION
+
+Plack::App::uWSGI is
+
+=head1 AUTHOR
+
+hiratara E<lt>hiratara@cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
